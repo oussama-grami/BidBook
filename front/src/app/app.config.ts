a4277528e35633a5a4432d0f68344b6bdf853a1a
Dhouibi Mohamed Aziz <mohamedaziz.dhouibi@insat.ucar.tn>
2025-04-09 15:31:29 +0100

    fixed ui

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -4,6 +4,8 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { MessageService } from 'primeng/api';
+import { providePrimeNG } from 'primeng/config';
+import { MyPreset } from '../mytheme';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +13,13 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     provideAnimations(),
     MessageService,
+    providePrimeNG({
+      theme: {
+        preset: MyPreset,
+        options: {
+          darkModeSelector: '.dark-theme',
+        },
+      }
+    })
   ],
 };
