6a3a25af53195e15087b7fc58c4b375689a5bb85
hiba <hibarakia@gmail.com>
2025-04-15 22:41:09 +0100

    Adding database configuration

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -4,11 +4,54 @@ import { AppService } from './app.service';
 import { config } from 'dotenv';
 import { GlobalModule } from './Common/global.module';
 import { AuthModule } from './auth/auth.module';
+import { Book } from './books/entities/book.entity';
+import { User } from './auth/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Favorite } from './favorites/entities/favorite.entity';
+import { Bid } from './bids/entities/bid.entity';
+import { Comment } from './comments/entities/comment.entity';
+import { Notification } from './notifications/entities/notification.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config'; // Ajout de ConfigService
 
 config({ path: `${process.cwd()}/Config/.env` });
 
 @Module({
-  imports: [GlobalModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ // Configuration de ConfigModule
+      isGlobal: true,
+      envFilePath: [
+        `${process.cwd()}/Config/.env`,
+        `${process.cwd()}/Config/.env.${process.env.NODE_ENV}`,
+      ],
+    }),
+    TypeOrmModule.forRootAsync({ // Configuration de TypeOrmModule
+      inject: [ConfigService],
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: configService.get('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USER'),
+          password: configService.get<string>('DB_PASS'),
+          database: configService.get<string>('DB_NAME'),
+          autoLoadEntities: true,
+          synchronize: configService.get<string>('NODE_ENV') === 'dev',
+          logging: true,
+        };
+      },
+    }),
+    GlobalModule,
+    AuthModule,
+    TypeOrmModule.forFeature([
+      User,
+      Book,
+      Favorite,
+      Comment,
+      Bid,
+      Notification
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
