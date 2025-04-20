c2965902885a2c75bc2e98a78918beb303a974ca
LeithEng <leithengazzou@gmail.com>
2025-04-20 17:12:39 +0100

    added create bid dto

--- back/src/bids/dto/create-bid.dto.ts
+++ back/src/bids/dto/create-bid.dto.ts
@@ -1,1 +1,12 @@
-export class CreateBidDto {}
+import { IsInt, IsPositive } from 'class-validator';
+
+export class CreateBidDto {
+    @IsInt()
+    userId: number;
+
+    @IsInt()
+    bookId: number;
+
+    @IsPositive()
+    amount: number;
+}
