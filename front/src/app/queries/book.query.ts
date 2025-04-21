05f49707dabfb117da24a2b083068eec926df274
LeithEng <leithengazzou@gmail.com>
2025-04-21 15:29:58 +0100

    added graphql config to the frontend

---
+++ front/src/app/queries/book.query.ts
@@ -0,0 +1,23 @@
+import { gql } from 'apollo-angular';
+
+export const VIEW_BOOKS_QUERY = gql`
+  query ViewBooks($limit: Int, $offset: Int) {
+    viewBooks(limit: $limit, offset: $offset) {
+      id
+      title
+      picture
+      rating {
+        rate
+      }
+      favorites {
+        id
+      }
+      bids {
+        id
+      }
+      category
+
+
+    }
+  }
+`;
