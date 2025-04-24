0ed2006a638214ad09aaea672a2865fced120710
LeithEng <leithengazzou@gmail.com>
2025-04-24 21:18:10 +0100

    fixed a small issue that caused an error when liking a book

--- back/src/books/books.service.ts
+++ back/src/books/books.service.ts
@@ -68,7 +68,7 @@ export class BooksService {
   async findOne(id: number): Promise<Book> {
     const book = await this.bookRepository.findOne({
       where: { id },
-      relations: ['owner', 'comments', 'bids','favorites','ratings', 'ratings.user'],
+      relations: ['owner', 'comments', 'bids','favorites','ratings', 'ratings.user', 'favorites.user'],
     });
 
     if (!book) {
